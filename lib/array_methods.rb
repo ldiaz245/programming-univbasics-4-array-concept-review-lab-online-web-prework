f505989f5b54aac607689ea6047c93d5e11cd275
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-08-03 17:13:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
-  (["0,1,2,3,4,5,6,7,8,9,10"])
+  (["1,2,1,3,4,3,5,4,2,1"])
 end
 
 def find_max_value(array)
-  (["1,2,1,3,4,3,5,4,3,2,1"])
+  
 end
 
 def find_min_value(array)
-  (["10,5,3,7,1,9,1,3,10"])
+  
 end
